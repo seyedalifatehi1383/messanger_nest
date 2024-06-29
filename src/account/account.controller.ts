f7c7b48c367e03602e5394c083f89568606da280
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-06-29 13:22:07 +0330

    in account controller: fix removeAccount controller

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -29,7 +29,7 @@ export class AccountController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.accountService.remove(+id);
+  removeAccount(@Param('id') id: string) {
+    return this.accountService.removeAccount(+id);
   }
 }
