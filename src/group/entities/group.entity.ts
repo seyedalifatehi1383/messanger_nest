384c612e4b9633f80bbfc0aeb8c7b1dba108f6f6
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-06-30 12:30:58 +0330

    create group entity

--- src/group/entities/group.entity.ts
+++ src/group/entities/group.entity.ts
@@ -1,1 +1,28 @@
-export class Group {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString, IsUUID } from "class-validator";
+
+export class Group {
+    @ApiProperty({ description: 'account PK', example: 1 })
+    @IsNumber()
+    ID: number;
+  
+    @ApiProperty({ description: 'account name', example: 'accountName' })
+    @IsString()
+    name: string;
+  
+    @ApiProperty({
+      description: 'account profile image',
+      example: '996de063-3f1b-4201-b9a1-2da6b32f09c5',
+    })
+    @IsUUID()
+    profile: string;
+  
+    @ApiProperty({ description: 'account userId', example: 1 })
+    @IsString()
+    GroupID: string;
+  
+    @ApiProperty({ description: 'is the user is Online', example: false })
+    @IsNumber()
+    messagesNumber: number;
+  
+}
