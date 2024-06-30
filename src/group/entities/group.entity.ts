6e6d462b77f3289d48f03a5b8f0490895c58842c
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-06-30 12:37:58 +0330

    fix the descriptions of group entity

--- src/group/entities/group.entity.ts
+++ src/group/entities/group.entity.ts
@@ -2,26 +2,26 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString, IsUUID } from "class-validator";
 
 export class Group {
-    @ApiProperty({ description: 'account PK', example: 1 })
+    @ApiProperty({ description: 'group PK', example: 1 })
     @IsNumber()
     ID: number;
   
-    @ApiProperty({ description: 'account name', example: 'accountName' })
+    @ApiProperty({ description: 'group name', example: 'groupName' })
     @IsString()
     name: string;
   
     @ApiProperty({
-      description: 'account profile image',
+      description: 'group profile image',
       example: '996de063-3f1b-4201-b9a1-2da6b32f09c5',
     })
     @IsUUID()
     profile: string;
   
-    @ApiProperty({ description: 'account userId', example: 1 })
+    @ApiProperty({ description: 'group id', example: '@group' })
     @IsString()
     GroupID: string;
   
-    @ApiProperty({ description: 'is the user is Online', example: false })
+    @ApiProperty({ description: 'the number of each messege of the group', example: 1 })
     @IsNumber()
     messagesNumber: number;
   
