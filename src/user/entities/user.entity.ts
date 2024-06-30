2c366fb13d951ea0bd928921f42ceaaee0ccc31c
Seyed Ali Fatehi <afatehi07@gmail.com>
2024-06-30 08:59:09 +0330

    create user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+export class User {
+  @ApiProperty({ description: 'user PK', example: 1 })
+  @IsNumber()
+  ID: number;
+
+  @ApiProperty({ description: 'username', example: 'username' })
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @ApiProperty({ description: 'email', example: 'example@email.com' })
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description: 'the password of user',
+    example: 'exp_pass',
+  })
+  @IsNotEmpty()
+  @IsStrongPassword()
+  password: string;
+}
